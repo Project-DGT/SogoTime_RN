eb2d7bf42138b621732cff08f7dedbea5387a68e
8954sood <8954sood@naver.com>
2024-11-15 10:40:50 +0900

    feat: Create Timetable Auto Scroll

--- feature/main/index.tsx
+++ feature/main/index.tsx
@@ -1,11 +1,35 @@
 import { Button, Image, ScrollView, StyleSheet, Text, Touchable, TouchableOpacity, View } from "react-native"
 import GearIcon from "../../assets/ic_gear.png"
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const MainScreen = () => {
 
   const [selectedWeekday, setSelectedWeekday] = useState("월요일 / Mon");
-  
+  // const []
+  const scrollRef = useRef<ScrollView>(null);
+  const [itemsScrollIndex, setItemScrollIndex] = useState<number[]>([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+
+  setTimeout(() => {
+    if (!scrollRef.current) return
+    console.log(itemsScrollIndex);
+    
+
+    const getIndex = 1;
+    let offsetY = 0.0;
+    for (let i = 0; i < getIndex; i++) {
+      if (i == 0) {
+        offsetY + 16;
+      }
+      offsetY += itemsScrollIndex[i];
+      offsetY += 16;
+    }
+    console.log(offsetY);
+    
+ 
+    scrollRef.current.scrollTo({y: offsetY, animated: true})
+  }, 1000);
+   
   return (
     <View style={styles.screen}>
       <View style={styles.headerContainer}>
@@ -23,23 +47,35 @@ const MainScreen = () => {
           <WeekdayCard
             text={weekday} 
             isSelect={weekday === selectedWeekday}
-             onClick={() => setSelectedWeekday(weekday)}
+            onClick={() => setSelectedWeekday(weekday)}
           />
         ))}
       </View>
 
       <View style={{height: 24}}/>
       <ScrollView 
+        ref={scrollRef}
         style={styles.timetableContainer} 
         contentContainerStyle={styles.timetableContent}
       >
-        <TimetableCard header="1교시 | 오전 08 : 50" text="테스트" isSelect={true} />
-        <TimetableCard header="1교시 | 오전 08 : 50" text="테스트" isSelect={false} />
-        <TimetableCard header="1교시 | 오전 08 : 50" text="테스트" isSelect={false} />
-        <TimetableCard header="1교시 | 오전 08 : 50" text="테스트" isSelect={false} />
-        <TimetableCard header="1교시 | 오전 08 : 50" text="테스트" isSelect={false} />
-        <TimetableCard header="1교시 | 오전 08 : 50" text="테스트" isSelect={false} />
-        <TimetableCard header="1교시 | 오전 08 : 50" text="테스트" isSelect={false} />
+        {/* onSetHeight={(height) => console.log(height)} */}
+        {[
+          "1교시 | 오전 08 : 50",
+          "2교시 | 오전 09 : 50", 
+          "3교시 | 오전 10 : 50", 
+          "4교시 | 오전 11 : 50",
+          "5교시 | 오후 13 : 30ewpreqwrmqwkeqwqmrlew\nqweqe\nqewqwe\nqwe\nqwemrlqwlrmlwqrmwqr", 
+          "6교시 | 오후 14 : 30",
+          "7교시 | 오후 15 : 30"].map((header, index) => (
+            <TimetableCard onSetHeight={(height) => {
+              const itemScrollIndex = itemsScrollIndex;
+              itemScrollIndex[index] = height;
+              setItemScrollIndex(itemScrollIndex);
+              console.log(itemScrollIndex);
+            }} header={header} text="테스트" isSelect={header === "1교시 | 오전 08 : 50"}/>
+          ))
+          }
+          <View style={{height: 400}}/>
       </ScrollView>
     </View>
   )
@@ -53,7 +89,7 @@ interface WeekdayCardProps {
 
 const WeekdayCard = ({text, isSelect, onClick}: WeekdayCardProps ) => {
   return (
-    <TouchableOpacity 
+    <TouchableOpacity
       style={{
         backgroundColor: isSelect ? 'rgba(73, 226, 150, 0.45);' : 'rgba(214, 214, 214, 0.29)',
         borderRadius: 25,
@@ -75,20 +111,27 @@ const WeekdayCard = ({text, isSelect, onClick}: WeekdayCardProps ) => {
 interface TimetableCardProps {
   header: string,
   text: string,
-  isSelect: boolean
+  isSelect: boolean,
+  onSetHeight: (height: number) => void,
 }
 
-const TimetableCard= ({header, text, isSelect}: TimetableCardProps) => {
+const TimetableCard= ({header, text, isSelect, onSetHeight}: TimetableCardProps) => {
   return (
-    <View style={{
-      width: '100%',
-      backgroundColor: isSelect? "rgba(215, 248, 232, 0.54)": "#EDEDED",
-      borderRadius: 24,
-      boxShadow: "2px 5px 3px 0px rgba(136, 134, 144, 0.20)",
-      paddingVertical: 19.5,
-      paddingStart: 20,
-      flexDirection: 'column',
-    }}>
+    <View 
+      onLayout={(event) => {
+        console.log("hi");
+        
+        onSetHeight(event.nativeEvent.layout.height);
+      }}
+      style={{
+        width: '100%',
+        backgroundColor: isSelect? "rgba(215, 248, 232, 0.54)": "#EDEDED",
+        borderRadius: 24,
+        boxShadow: "2px 5px 3px 0px rgba(136, 134, 144, 0.20)",
+        paddingVertical: 19.5,
+        paddingStart: 20,
+        flexDirection: 'column',
+      }}>
       <Text style={{
         fontSize: 10,
         fontWeight: 500,
@@ -119,7 +162,7 @@ const TimetableCard= ({header, text, isSelect}: TimetableCardProps) => {
 const styles = StyleSheet.create({
   screen: {
     height: '100%',
-    width: '100%',
+    width: '100%', 
     flexDirection: 'column',
     // backgroundColor: 'red',
   },
