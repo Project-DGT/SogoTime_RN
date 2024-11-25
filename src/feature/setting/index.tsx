e54ac0277646d1fd4e5e0ed1e149c3f0a1f357d7
8954sood <8954sood@naver.com>
2024-11-25 15:04:35 +0900

    feat: Create Setting Screen

--- src/feature/setting/index.tsx
+++ src/feature/setting/index.tsx
@@ -3,35 +3,100 @@ import ArrowLeftIcon from "../../../assets/ic_arrow_left.png";
 import AvatarIcon from "../../../assets/ic_avatar.png";
 import ChevronRightIcon from "../../../assets/ic_chevron_right.png";
 import { isEnabled } from "react-native/Libraries/Performance/Systrace";
+import React, { useCallback, useEffect, useMemo, useRef } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import BottomSheet, { BottomSheetBackdrop, BottomSheetModal, BottomSheetModalProvider, BottomSheetView } from '@gorhom/bottom-sheet';
+import { DodamTextField } from "../../component/textfield";
+import SogoTimeButton from "../../component/button";
+import Animated, { interpolate } from "react-native-reanimated";
 
 const SettingScreen = () => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.topbarContainer}>
-        <TouchableHighlight
-          style={styles.topbarIcon}
-          underlayColor={'rgba(0, 0, 0, 0.08)'}
-          onPress={() => {}}
-        >
-          <Image source={ArrowLeftIcon}/>
-        </TouchableHighlight>
-        <Text style={styles.topbarTitle}>설정</Text>
-      </View>
-      <View style={styles.contentContainer}>
-        <View style={styles.profileContainer}>
-          <Image source={AvatarIcon} style={styles.profileImage}/>
-          <View style={styles.profileTextContainer}>
-            <Text style={styles.profileText}>1학년 3반</Text>
-            <Text style={styles.profileEditText}>정보 수정</Text>
-          </View>
-        </View>
+  const renderBackdrop = useCallback(
+    (props: any) => <BottomSheetBackdrop {...props} pressBehavior="close" />,
+    [],
+  );
 
-        <View style={styles.divider}></View>
-        <SettingItem title="서비스 운영 정책" onPress={() => {}}/>
-        <SettingItem title="개인정보 처리 방침" onPress={() => {}}/>
-        <SettingItem title="버전" description="1.0.0"/>
-      </View>
-    </View>
+  // ref
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+
+  // variables
+  const snapPoints = useMemo(() => ['25%', '50%'], []);
+
+  // callbacks
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index);
+  }, []);
+
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <BottomSheetModalProvider>
+        <View style={styles.topbarContainer}>
+          <TouchableHighlight
+            style={styles.topbarIcon}
+            underlayColor={'rgba(0, 0, 0, 0.08)'}
+            onPress={() => {}}
+          >
+            <Image source={ArrowLeftIcon}/>
+          </TouchableHighlight>
+          <Text style={styles.topbarTitle}>설정</Text>
+        </View>
+        <View style={styles.contentContainer}>
+          <View style={styles.profileContainer}>
+            <Image source={AvatarIcon} style={styles.profileImage}/>
+            <View style={styles.profileTextContainer}>
+              <Text style={styles.profileText}>1학년 3반</Text>
+              <TouchableHighlight
+                style={{
+                  backgroundColor: "#ffffff",
+                  borderRadius: 25,
+                }} 
+                underlayColor={'rgba(0, 0, 0, 0.08)'}
+                onPress={handlePresentModalPress}>
+              <Text style={styles.profileEditText}>정보 수정</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+
+          <View style={styles.divider}></View>
+          <SettingItem title="서비스 운영 정책" onPress={() => {}}/>
+          <SettingItem title="개인정보 처리 방침" onPress={() => {}}/>
+          <SettingItem title="버전" description="1.0.0"/>
+        </View>
+        <BottomSheetModal
+          ref={bottomSheetModalRef}
+          index={1}
+          snapPoints={snapPoints}
+          onChange={handleSheetChanges}
+          backdropComponent={renderBackdrop} 
+        >
+          <BottomSheetView style={styles.bottomSheetContainer}>
+            {/* <View style={styles.bottomSheetHandleContainer}>
+              <View style={styles.bottomSheetHandleIndicator}></View>
+            </View> */}
+            <DodamTextField
+              label="학생정보"
+              placeholder="이름을 입력해주세요"
+              value="1학년 4반"
+              onValueChange={(value => {
+
+              })}/>
+
+            <View style={{
+              height: 24,
+            }}/>
+            
+            <SogoTimeButton
+              text="저장하기"
+              onPress={() => {}}/>
+            
+
+          </BottomSheetView>
+        </BottomSheetModal>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   )
 }
 
@@ -151,7 +216,29 @@ const styles = StyleSheet.create({
     width: 14,
     height: 14,
     tintColor: '#747678',
-  }
+  },
+  backdrop: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'black', // 어두운 배경 색상
+  },
+  bottomSheetContainer: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    paddingHorizontal: 16,
+    gap: 24
+  },
+  bottomSheetHandleContainer: {
+    paddingVertical: 12,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bottomSheetHandleIndicator: {
+    width: 76,
+    height: 2,
+    backgroundColor: '#000000',
+  },
 })
 
 
