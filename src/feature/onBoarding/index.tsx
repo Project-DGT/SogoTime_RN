25b06254f55e29ee892c0ab9a6155f5fb4f6ead5
8954sood <8954sood@naver.com>
2024-12-01 18:50:24 +0900

    chore: View Position

--- src/feature/onBoarding/index.tsx
+++ src/feature/onBoarding/index.tsx
@@ -13,7 +13,7 @@ export default function OnBoardingScreen() {
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.textContainer}>
         <Text style={styles.subTitle}>시간표를</Text>
-        <Text style={styles.subTitle}>빠르고 간편하게</Text>
+        <Text style={styles.subTitle}>빠르고 간편하게,</Text>
         <Text style={styles.mainTitle}>소고타임</Text>
       </View>
       <TouchableOpacity style={styles.startButton} onPress={() => navigation.navigate('InputScreen')}>
@@ -33,10 +33,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFFFFF"
   },
   textContainer: {
+    alignSelf: "flex-start",
     alignItems: "flex-start",
     paddingStart: 24,
     paddingTop: 95,
-    paddingEnd: 120,
     marginBottom: 'auto',
   },
   subTitle: {
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
     fontSize: 68,
     color: '#3FD289',
     fontWeight: '900',
-    paddingTop: 15,
+    marginTop: 15,
   },
   startButton: {
     backgroundColor: '#3FD289',
